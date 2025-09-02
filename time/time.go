929e64a398a78f644e652a8b12da8f86102398ea
BaiKangrui <lance4117@outlook.com>
2025-09-02 14:50:02 +0800

    ADD time

---
+++ time/time.go
@@ -0,0 +1,42 @@
+package time
+
+import "time"
+
+const (
+	Nanosecond  int64 = 1
+	Microsecond       = 1000 * Nanosecond
+	Millisecond       = 1000 * Microsecond
+	Second            = 1000 * Millisecond
+	Minute            = 60 * Second
+	Hour              = 60 * Minute
+)
+
+// NowMilli 当前时间的毫秒时间戳
+func NowMilli() int64 {
+	return time.Now().UnixMilli()
+}
+
+// NowDateTime 当前日期时间
+func NowDateTime() string {
+	return time.Now().Format(time.DateTime)
+}
+
+// NowDateOnly 当前日期，没有时间
+func NowDateOnly() string {
+	return time.Now().Format(time.DateOnly)
+}
+
+// NowAfter 当前时间变化后的毫秒时间戳，负数是以前
+func NowAfter(dur int64) int64 {
+	return time.Now().Add(time.Duration(dur)).UnixMilli()
+}
+
+// ToDateTime 将毫秒时间戳变为时间日期格式
+func ToDateTime(milliseconds int64) string {
+	return time.UnixMilli(milliseconds).Format(time.DateTime)
+}
+
+// ToDateOnly 将毫秒时间戳变为年月日格式
+func ToDateOnly(milliseconds int64) string {
+	return time.UnixMilli(milliseconds).Format(time.DateOnly)
+}
---
+++ time/time_test.go
@@ -0,0 +1,19 @@
+package time
+
+import (
+	"testing"
+)
+
+func TestTime(t *testing.T) {
+	now := NowMilli()
+	t.Log(now)
+	nowafter := NowAfter(-Hour)
+	t.Log(nowafter)
+	t.Log(ToDateTime(now))
+	t.Log(ToDateTime(nowafter))
+	t.Log(ToDateOnly(nowafter))
+
+	t.Log(NowDateTime())
+	t.Log(NowDateOnly())
+
+}
