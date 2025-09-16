3c218f2ded1de1a0a39a0f47a75d693d93f20316
lance <lance4117@outlook.com>
2025-09-17 00:30:23 +0800

    MOD utils.cmd

---
+++ utils/cmd.go
@@ -0,0 +1,26 @@
+package utils
+
+import (
+	"bytes"
+	"errors"
+	"os/exec"
+)
+
+// ExactCmd 执行一个外部命令
+// 注意，这里的arg必须要分开
+// 这里并不会像 shell 那样帮你解析一整条字符串
+func ExactCmd(name string, arg ...string) error {
+	var out bytes.Buffer
+
+	cmd := exec.Command(name, arg...)
+
+	out.Reset()
+	cmd.Stdout = &out
+	cmd.Stderr = &out
+
+	err := cmd.Run()
+	if err != nil {
+		return errors.New(out.String())
+	}
+	return nil
+}
